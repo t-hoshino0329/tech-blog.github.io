4b26b854d65e81c8fde96b3f7dab1cfe826900e3
t-hoshino0329 <hoshino.taisei0329@gmail.com>
2026-06-09 02:26:17 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,3 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL =
+  "https://t-hoshino0329.github.io/tech-blog.github.io/";
